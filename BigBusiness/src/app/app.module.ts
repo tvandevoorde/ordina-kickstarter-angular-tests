a4ecc8cba319dfe8d650ce6891c20188140be83d
Tom <tom.vandevoorde@gmail.com>
2019-08-18 12:50:14 +0200

    Added navbar

--- BigBusiness/src/app/app.module.ts
+++ BigBusiness/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ProductCatalogComponent } from './product-catalog/product-catalog.compo
 import { CatalogItemComponent } from './catalog-item/catalog-item.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
 import { ShoppingCartItemComponent } from './shopping-cart-item/shopping-cart-item.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { ShoppingCartItemComponent } from './shopping-cart-item/shopping-cart-it
     ProductCatalogComponent,
     CatalogItemComponent,
     ShoppingCartComponent,
-    ShoppingCartItemComponent
+    ShoppingCartItemComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
